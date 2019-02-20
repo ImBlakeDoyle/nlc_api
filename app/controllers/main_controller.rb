96b35273b5815bda5672ba3f16520383c373665d
Blake <blake.doyle@hotmail.com.au>
2019-02-20 11:04:18 +1100

    post content in form and display the content

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,11 @@
 class MainController < ApplicationController
+
+    def display
+        @content = form_params["content"]
+    end
+
+    private
+    def form_params
+        params.permit(:content)
+    end
 end
