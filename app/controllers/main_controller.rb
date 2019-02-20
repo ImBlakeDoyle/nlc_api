89eb928623322c09ac8a33ffdf8479b104f90776
Blake <blake.doyle@hotmail.com.au>
2019-02-20 15:22:37 +1100

    move credentials

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,25 +4,25 @@ require 'json'
 class MainController < ApplicationController
 
     def display
-        text = form_params["content"]
+        @text = form_params["content"]
 
         natural_language_classifier = NaturalLanguageClassifierV1.new(
-        iam_apikey: "9ToG0vwSe4sOGzOSFqw7qct0WDNBBA33AGNKc03CJ7hq",
-        url: "https://gateway-tok.watsonplatform.net/natural-language-classifier/api"
+        iam_apikey: Rails.application.credentials.nlc_api,
+        url: Rails.application.credentials.nlc_url
         )
 
         categories = natural_language_classifier.classify(
-        classifier_id: "083e4dx38-nlc-3",
-        text: text
+        classifier_id: Rails.application.credentials.classifier_id,
+        text: @text
         )
 
 
-        # p categories.result["top_class"]
+        puts categories.result
 
-        @content = categories.result["top_class"]
+        @content = categories.result
+        @topclass = categories.result["top_class"]
         @confidence = categories.result["classes"][0]["confidence"]
 
-        # @content = JSON.pretty_generate(categories.result)
     end
 
     private
